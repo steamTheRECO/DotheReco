3c3fa291456d89d781e2d43448296f89911efb1b
Kim-jihoo <jihoo1106@ewhain.net>
2024-02-23 21:11:38 +0900

    Initial commit

---
+++ src/main/java/com/dothereco/DotheReco/DotheRecoApplication.java
@@ -0,0 +1,13 @@
+package com.dothereco.DotheReco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotheRecoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotheRecoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dothereco/DotheReco/DotheRecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dothereco.DotheReco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DotheRecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
