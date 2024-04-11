c68168671c92531e8954fb4254f2b26d2dfd4a88
JeonJaeeun <jane9518@naver.com>
2024-04-12 07:23:19 +0900

    demo 완료

--- src/main/frontend/src/LoginPage.jsx
+++ src/main/frontend/src/LoginPage.jsx
@@ -12,16 +12,16 @@ function LoginPage() {
             <button onClick={() => window.history.back()} className="back-button" id="back-button">
                 &lt;
             </button>
-            <h1>Login</h1>
+            <h1 className="login-h1">Login</h1>
             <hr />
             <form onSubmit={handleSubmit}>
-                <div className="input-container">
+                <div className="login-input-container">
                     <label htmlFor="id-account"></label>
-                    <input type="text" name="account" id="id-account" placeholder="이메일" required />
+                    <input type="text" className="login-account" id="id-account" placeholder="이메일" required />
                 </div>
-                <div className="input-container">
+                <div className="login-input-container">
                     <label htmlFor="password-account"></label>
-                    <input type="password" name="password" id="password-account" placeholder="비밀번호" required />
+                    <input type="password" className="login-password" id="password-account" placeholder="비밀번호" required />
                 </div>
                 <button className="login-button" type="submit">확인</button>
             </form>
