9921a7744375bfebf4c49102e87869621032ecee
Yae Eun <stulee12356@gmail.com>
2024-05-22 05:35:23 +0900

    장소검색-app.js수정

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -4,13 +4,13 @@ import StartPage from './StartPage';
 import SignupPage from './SignupPage';
 import LoginPage from './LoginPage';
 import AddNormalSchedulePage from "./AddNormalSchedulePage";
-import AddFlexSchedulePage from "./AddFlexSchedulePage";
+//import AddFlexSchedulePage from "./AddFlexSchedulePage";
 import Main from "./Main"
-import TimeLinePage from "./TimeLinePage";
-import TimePicker from "./TimePicker";
-import ToDoListPage from "./ToDoListPage";
-import RecommendationPage from "./RecommendationPage";
-import SettingPage from "./SettingPage";
+//import TimeLinePage from "./TimeLinePage";
+//import TimePicker from "./TimePicker";
+//import ToDoListPage from "./ToDoListPage";
+//import RecommendationPage from "./RecommendationPage";
+//import SettingPage from "./SettingPage";
 import TmapComponent from "./TmapComponent";
 
 function App() {
@@ -21,14 +21,9 @@ function App() {
                 <Route path="/signup" element={<SignupPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/addNormalschedule" element={<AddNormalSchedulePage/>} />
-                <Route path="/addFlexschedule" element={<AddFlexSchedulePage/>} />
+
                 <Route path="/Main" element={<Main/>}/>
-                <Route path="/TimeLine" element={<TimeLinePage/>} />
-                <Route path="/TimePicker" element={<TimePicker/>} />
-                <Route path="/ToDoList" element={<ToDoListPage />} />
-                <Route path="/Recommendation" element={<RecommendationPage/>} />
-                <Route path="/Setting" element={<SettingPage/>} />
-                <Route path="/map" element={<TmapComponent />} /> {/* TmapComponent 추가 */}
+                <Route path="/Map" element={<TmapComponent/>} /> {/* TmapComponent 추가 */}
             </Routes>
         </Router>
     );
